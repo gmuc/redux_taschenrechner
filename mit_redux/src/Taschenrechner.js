850118789c1404b2fd3708dbbfee490cb881fc77
Gerd Mucha <Gerd.Mucha@br.de>
2018-08-17 13:23:42 +0200

    erste funktionsfähige mit_redux Version

--- mit_redux/src/Taschenrechner.js
+++ mit_redux/src/Taschenrechner.js
@@ -1,37 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import * as Actions from './actions';
 
+class Taschenrechner extends React.Component {
+	plus(wert1, wert2, updateErgebnis) {
+		let summe = Number(wert1) + Number(wert2);
 
-export default class Taschenrechner extends React.Component {
+		let gleichung = wert1 + ' + ' + wert2 + ' = ' + summe;
 
-	plus(wert1,wert2,updateErgebnis) {
-		let summe = Number(wert1()) + Number(wert2());
-
-		let gleichung = wert1() + ' + ' + wert2() + ' = ' + summe;
-
-        updateErgebnis(gleichung);
+		updateErgebnis(gleichung);
 	}
 
-	minus(wert1,wert2,updateErgebnis) {
+	minus(wert1, wert2, updateErgebnis) {
+		let differenz = Number(wert1) - Number(wert2);
 
-		let differenz = Number(wert1()) - Number(wert2());
+		let gleichung = wert1 + ' - ' + wert2 + ' = ' + differenz;
 
-		let gleichung = wert1() + ' - ' + wert2() + ' = ' + differenz;
-
-        updateErgebnis(gleichung);
-    }
+		updateErgebnis(gleichung);
+	}
 
 	render() {
-        const { wert1, updateWert1, resetWert1, wert2, updateWert2, resetWert2, ergebnis, updateErgebnis, resetErgebnis } = this.props;
+		const {
+			wert1,
+			updateWert1,
+			resetWert1,
+			wert2,
+			updateWert2,
+			resetWert2,
+			ergebnis,
+			updateErgebnis,
+			resetErgebnis,
+		} = this.props;
 
-        return (
+		return (
 			<div>
 				Wert1:{'  '}
 				<input
 					onChange={event => {
-                        updateWert1(event.target.value);
-                        resetErgebnis();
+						updateWert1(event.target.value);
+						resetErgebnis();
 					}}
 					value={wert1}
 				/>
@@ -42,8 +50,8 @@ export default class Taschenrechner extends React.Component {
 				Wert2:{'  '}
 				<input
 					onChange={event => {
-                        updateWert2(event.target.value);
-                        resetErgebnis();
+						updateWert2(event.target.value);
+						resetErgebnis();
 					}}
 					value={wert2}
 				/>
@@ -51,26 +59,33 @@ export default class Taschenrechner extends React.Component {
 				= {wert2}
 				<br />
 				<br />
-				<button onClick={() => plus(wert1,wert2,updateErgebnis)}>+</button>
+				<button onClick={() => this.plus(wert1, wert2, updateErgebnis)}>
+					+
+				</button>
 				{'  '}
-				<button onClick={() => minus(wert1,wert2,updateErgebnis)}>-</button>
+				<button onClick={() => this.minus(wert1, wert2, updateErgebnis)}>
+					-
+				</button>
 				<br />
-				<p>Ergebnis: {'  ' + ergebnis()}</p>
+				<p>Ergebnis: {'  ' + ergebnis}</p>
 			</div>
 		);
 	}
 }
 
 function mapStateToProps(state) {
-    return {
-        wert1: state.wert1,
-        wert2: state.wert2,
-        ergebnis: state.ergebnis
-    };
+	return {
+		wert1: state.wert1,
+		wert2: state.wert2,
+		ergebnis: state.ergebnis,
+	};
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(Actions, dispatch);
+	return bindActionCreators(Actions, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Taschenrechner);
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(Taschenrechner);
